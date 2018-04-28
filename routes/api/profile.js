2f8b45b2110addbcd1dd5d2adb3a7b35964e5c3f
Mirza Medar <mirza.medar@edu.fit.ba>
2018-04-28 16:02:55 +0200

    Profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
-
+const validateProfileInput = require("../../validation/profile");
 // Load Profile model
 const Profile = require("../../models/Profile");
 // Load User model
@@ -25,6 +25,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user!";
@@ -44,6 +45,12 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
