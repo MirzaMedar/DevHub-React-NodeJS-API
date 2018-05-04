790b3d32ed855d87c2abb4d7d05fd74d5969ba92
Mirza Medar <mirza.medar@edu.fit.ba>
2018-05-04 21:54:23 +0200

    POST and DELETE methods for comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -145,4 +145,67 @@ router.post(
   }
 );
 
+// @route POST api/posts/comment/:id
+// @desc Add comment to post
+// @access Private
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found!" }));
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Remove comment from post
+// @access Private
+
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          //comment does not exists
+          return res
+            .status(404)
+            .json({ commentnotfound: "Comment not found!" });
+        } else {
+          const removeIndex = post.comments
+            .map(item => item._id.toString())
+            .indexOf(req.params.comment_id);
+
+          post.comments.splice(removeIndex, 1);
+
+          post.save().then(post => res.json(post));
+        }
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found!" }));
+  }
+);
+
 module.exports = router;
